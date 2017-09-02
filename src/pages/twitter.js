7d3d1da8d17f072729f6acf8a491769e90a2cc60
turt2live <travpc@gmail.com>
2017-09-02 16:37:26 -0600

    Add twitter bridge documentation

---
+++ src/pages/twitter.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TwitterPage = () => (
+    <div>
+        <h2>Twitter Bridge</h2>
+        <p>The Twitter bridge allows you to follow your favorite people and brands right inside of Matrix/Riot.</p>
+        <p>To follow someone's timeline, or a hashtag, join the special room: <code>#_twitter_@ThePerson:t2bot.io</code> or <code>#_twitter_#TheHashtag:t2bot.io</code></p>
+        <br/>
+        <br/>
+        <br/>
+        <p>To learn more about the bridge, or to contribute, please see <a href="https://github.com/Half-Shot/matrix-appservice-twitter">Half-Shot/matrix-appservice-twitter</a> on Github.</p>
+    </div>
+);
+
+export default TwitterPage
