e3998b1359bbcb347e9cf6d3f8a829955cbe8d45
turt2live <travpc@gmail.com>
2017-09-02 16:47:57 -0600

    Add email bot documentation

---
+++ src/pages/emailbot.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const EmailBotPage = () => (
+    <div>
+        <h2>Email Bot</h2>
+        <p><a href="https://matrix.to/#/@email:t2bot.io">@email:t2bot.io</a> is a bot that posts a message when it receives an email. Currently this bot does not have self-service configuration, so please contact <a href="https://matrix.to/#/@travis:t2l.io">@travis:t2l.io</a> to set it up in a room.</p>
+        <br/>
+        <br/>
+        <br/>
+        <p>To learn more about the bot, or to contribute, please see <a href="https://github.com/turt2live/matrix-email-bot">turt2live/matrix-email-bot</a> on Github.</p>
+    </div>
+);
+
+export default EmailBotPage
