1db9c7f485823d0196d384aa02c8bea63ec5999b
turt2live <travpc@gmail.com>
2017-09-02 16:57:23 -0600

    Add pollbot documentation

---
+++ src/pages/pollbot.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Link from "gatsby-link";
+
+const PollBotPage = () => (
+    <div>
+        <h2>Pollbot</h2>
+            <div style={{background: '#ffdd2e', padding: '1.1rem', marginBottom: '1rem'}}><strong>The pollbot is beta!</strong> The bot may not work at times, or be slow to respond. Please visit <a href="https://matrix.to/#/#help:t2bot.io">#help:t2bot.io</a> if you run into any issues.</div>
+        <p><a href="https://matrix.to/#/@pollbot:t2bot.io">@pollbot:t2bot.io</a> helps run quick polls/votes in your room. To get started, invite the bot and send the message <code>!newpoll</code>.</p>
+        <br/>
+        <br/>
+        <br/>
+        <p>To learn more about the bot, or to contribute, please see <a href="https://github.com/shawnanastasio/matrix-pollbot">shawnanastasio/matrix-pollbot</a> on Github.</p>
+    </div>
+);
+
+export default PollBotPage
--- src/pages/twitter.js
+++ src/pages/twitter.js
@@ -3,6 +3,7 @@ import React from "react";
 const TwitterPage = () => (
     <div>
         <h2>Twitter Bridge</h2>
+            <div style={{background: '#ffdd2e', padding: '1.1rem', marginBottom: '1rem'}}><strong>Twitter bridging is beta!</strong> The bridge may not work at times, or may be difficult to set up. Please visit <a href="https://matrix.to/#/#help:t2bot.io">#help:t2bot.io</a> if you run into any issues.</div>
         <p>The Twitter bridge allows you to follow your favorite people and brands right inside of Matrix/Riot.</p>
         <p>To follow someone's timeline, or a hashtag, join the special room: <code>#_twitter_@ThePerson:t2bot.io</code> or <code>#_twitter_#TheHashtag:t2bot.io</code></p>
         <br/>
