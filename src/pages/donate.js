6dd4bf27337742a4714d9035db5d3d70e67d35e6
Travis Ralston <travpc@gmail.com>
2017-10-21 15:32:51 -0600

    Add a donation page

---
+++ src/pages/donate.js
@@ -0,0 +1,68 @@
+import React from "react";
+
+const DonatePage = () => (
+    <div>
+        <h2>Donations</h2>
+        <p>All donations are graciously appreciated. Donations help keep t2bot.io maintained and developers happy :)</p>
+        <div style={{position: "relative", margin: "5px"}}>
+            <a href="https://liberapay.com/turt2live/donate">
+                <img alt="Donate using Liberapay" src="https://liberapay.com/assets/widgets/donate.svg" style={{
+                    margin: 0
+                }}/>
+            </a>
+        </div>
+        <div style={{position: "relative", margin: "5px"}}>
+            <img src="/img/payments/bitcoin.svg" style={{
+                // "height: 30px; width: 30px; margin: 0; padding: 0; position: absolute; top: -3px; left: 0;"
+                height: "30px",
+                width: "30px",
+                margin: 0,
+                padding: 0,
+                position: "absolute",
+                top: "-1px",
+                left: 0
+            }} />
+            <code style={{
+                margin: 0,
+                padding: 0,
+                marginLeft: "35px"
+            }}>BTC 1LPZZkPH7ZbmWQxNhCrK38gkLrMYyA2Jbi</code>
+        </div>
+        <div style={{position: "relative", margin: "5px"}}>
+            <img src="/img/payments/ethereum.svg" style={{
+                // "height: 30px; width: 30px; margin: 0; padding: 0; position: absolute; top: -3px; left: 0;"
+                height: "30px",
+                width: "30px",
+                margin: 0,
+                padding: 0,
+                position: "absolute",
+                top: "-1px",
+                left: 0
+            }} />
+            <code style={{
+                margin: 0,
+                padding: 0,
+                marginLeft: "35px"
+            }}>ETH 0x4DdD2403607694b10A478dD9cAEaB9CAfB7F7449</code>
+        </div>
+        <div style={{position: "relative", margin: "5px"}}>
+            <img src="/img/payments/litecoin.svg" style={{
+                // "height: 30px; width: 30px; margin: 0; padding: 0; position: absolute; top: -3px; left: 0;"
+                height: "30px",
+                width: "30px",
+                margin: 0,
+                padding: 0,
+                position: "absolute",
+                top: "-1px",
+                left: 0
+            }} />
+            <code style={{
+                margin: 0,
+                padding: 0,
+                marginLeft: "35px"
+            }}>LTC LYRdNttmhJ1op5EUMiPsHzaZjriJxmL1J8</code>
+        </div>
+    </div>
+);
+
+export default DonatePage
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,7 @@ const IndexPage = () => (
     <div>
         <h2>A public bridge, bot, and integration network for <a href="https://matrix.org" target="_BLANK">[matrix]</a>
         </h2>
-        <p>Many of the bridges, bots, and integrations running on t2bot.io are free to use, although donations directly
-            to <a href="https://en.liberapay.com/turt2live">turt2live</a> are appreciated. All donations go towards
-            development and server maintenance.</p>
+        <p>Many of the bridges, bots, and integrations running on t2bot.io are free to use, although <Link to="/donate">donations are greatly appreciated</Link>.</p>
         <p>If you're unsure of how to set anything up, or have any questions/requests, please visit <a
             href="https://matrix.to/#/#help:t2bot.io">#help:t2bot.io</a> in matrix.</p>
 
