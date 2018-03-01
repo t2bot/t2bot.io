d967e83deddf14afc01a12828cb6b38d8a18cea4
Travis Ralston <travpc@gmail.com>
2018-03-01 10:30:07 -0700

    Change Discord documentation to reference bridging existing rooms

--- src/pages/discord.js
+++ src/pages/discord.js
@@ -3,15 +3,14 @@ import React from "react";
 const DiscordPage = () => (
     <div>
         <h2>Discord Bridge</h2>
-        <p>The Discord bridge requires the Guild/Server owner to approve the integration before bridging can occur.</p>
+        <p>The Discord bridge requires the Guild/Server owner to approve the integration for bridging to work.</p>
         <ol>
-            <li>Invite the bridge to your Discord guild: <a href="https://discordapp.com/api/oauth2/authorize?client_id=309408702530846730&scope=bot&permissions=607243264" target="_blank">Click Me</a>. The bot will need all of the permissions selected.</li>
-            <li>Open the room in the Discord app in your web browser</li>
-            <li>Add a webhook named <code>_matrix</code> to each room you'd like to bridge.</li>
+            <li>Invite the bridge to your Discord guild using <a href="https://discordapp.com/api/oauth2/authorize?client_id=309408702530846730&scope=bot&permissions=607243264" target="_blank">this link</a>. The bridge will need all the permissions selected.</li>
+            <li>Invite <a href="https://matrix.to/#/@_discord_bot:t2bot.io" target="_blank">@_discord_bot:t2bot.io</a> to your matrix room. The bot should have permissions to invite users.</li>
+            <li>Open the channel you'd like to bridge in the Discord web application</li>
             <li>
-                Join the Discord room in Matrix/Riot using the following as reference:
-                <img src="/img/discord-url.png" style={{display: "block"}}/>
-                The room ID would be #_discord_ChannelID_RoomID:t2bot.io
+                Using the following image as a reference, send the message <code>!discord bridge ChannelID RoomID</code> in your matrix room:
+                <img src="/img/discord-url.png" style={{display: "block"}} />
             </li>
         </ol>
         <br/>
