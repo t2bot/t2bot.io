f107e8b163d25edbbf1961a9d94b8dc3a24e6218
Travis Ralston <travpc@gmail.com>
2018-05-31 13:25:24 -0600

    Add a class for user pills

---
+++ src/layouts/user_pill.js
@@ -0,0 +1,26 @@
+import React from "react";
+import axios from 'axios';
+
+export default class UserPill extends React.Component {
+    componentWillMount() {
+        this.setState({profile: {}});
+        axios.get("https://t2bot.io/_matrix/t2bot/user_profile", {params: {user_id: this.props.userId}})
+            .then(response => {
+                console.log(response.data);
+                this.setState({profile: response.data});
+            });
+    }
+
+    render() {
+        const displayName = this.state.profile.displayname || this.props.userId;
+        const avatarMxc = this.state.profile.avatar_url || "mxc://t2l.io/UPAtGsUlQYsPwrtAbnSJYvHV";
+        const avatarUrl = "https://t2bot.io/_matrix/media/r0/thumbnail/" + avatarMxc.split('?')[0].substring("mxc://".length) + "?width=120&height=120&animated=true";
+
+        return (
+            <a className="t2_UserPill" href={"https://matrix.to/#/" + this.props.userId} title={this.props.userId}>
+                <img src={avatarUrl} className="t2_UserPill_avatar"/>
+                <span className="t2_UserPill_displayName">{displayName}</span>
+            </a>
+        );
+    }
+}
