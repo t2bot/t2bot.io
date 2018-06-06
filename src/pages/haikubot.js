55e6ca5ae9a5c76a757e22ac4506ce618ea3aff4
Travis Ralston <travpc@gmail.com>
2018-06-05 18:12:10 -0600

    Update haiku bot documentation

--- src/pages/haikubot.js
+++ src/pages/haikubot.js
@@ -1,14 +1,20 @@
 import React from "react";
-import Link from "gatsby-link";
+import UserPill from "../layouts/user_pill";
 
 const HaikuBotPage = () => (
     <div>
-        <h2>Haiku Bot</h2>
-        <p><a href="https://matrix.to/#/@haiku:t2bot.io">@haiku:t2bot.io</a> sits in your room and listens for messages that fit the haiku format. If a message matches, it prints it back out as a haiku.</p>
-        <br/>
-        <br/>
-        <br/>
-        <p>To learn more about the bot, or to contribute, please see <a href="https://github.com/turt2live/matrix-haiku-bot">turt2live/matrix-haiku-bot</a> on Github.</p>
+        <h2 className="t2-pageHeading">Haiku Bot</h2>
+        <p className="t2-repoInfo">
+            Github Repository: <a href="https://github.com/turt2live/matrix-haiku-bot">turt2live/matrix-haiku-bot</a><br/>
+        </p>
+        <p>
+            <UserPill userId="@haiku:t2bot.io"/> is a bot that detects haikus and posts them back into the room.
+        </p>
+        <h4>Adding the bot to your room</h4>
+        <ol>
+            <li>Invite <UserPill userId="@haiku:t2bot.io"/> to your room</li>
+            <li>Send a message that fits the haiku format</li>
+        </ol>
     </div>
 );
 
