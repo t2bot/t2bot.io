86fc8eb4537422a31659fce4d25a93f5a547f5b8
Travis Ralston <travpc@gmail.com>
2018-08-06 15:28:58 -0600

    Document telegram puppeting

--- src/pages/telegram.js
+++ src/pages/telegram.js
@@ -26,6 +26,19 @@ const TelegramPage = () => (
             <li>The bridge will ask you to confirm that you'd like to unbridge the room. Do this by sending the message <code>!tg confirm-unbridge</code></li>
             <li>The bridged users should automatically leave and the bridge will be severed</li>
         </ol>
+        <h4>Logging in with your Telegram account</h4>
+        <p>
+            By default the bridge uses a "relay bot" for people who are not logged in to the bridge. This can lead to confusion when the
+            Telegram bridge posts lots of messages in your Telegram chats. By using your own Telegram account, the bridge will send messages
+            to chats with your account rather than the relay bot when you talk on matrix. In addition to that, you also get invited to all
+            current and future Telegram chats - it's like using Telegram in Matrix!
+        </p>
+        <ol>
+            <li>Invite <UserPill userId="@telegram:t2bot.io"/> to a <strong>private chat</strong></li>
+            <li>Send the message <code>!tg login</code></li>
+            <li>Click the link and follow the instructions</li>
+            <li>The bridge will now start inviting you to all your Telegram chats and synchronize the rooms</li>
+        </ol>
     </div>
 );
 
