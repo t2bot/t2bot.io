320e06504a043fcdd691e7e84f5c9ab921aa9706
Tim Stahel <git@swedneck.xyz>
2018-12-05 23:51:19 +0100

    change info and links

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -3,15 +3,15 @@ import React from "react";
 const DonatePage = () => (
     <div>
         <h2>Donations</h2>
-        <p>All donations are graciously appreciated. Donations help keep t2bot.io maintained and developers happy :)</p>
+        <p>All donations are graciously appreciated. Donations help keep linuxgaming.life maintained and developers happy :)</p>
         <div style={{position: "relative", margin: "5px"}}>
-            <a href="https://liberapay.com/turt2live/donate">
-                <img alt="Donate using Liberapay" src="https://liberapay.com/assets/widgets/donate.svg" style={{
+            <a href="https://liberapay.com/Swedneck/donate">
+                <img alt="Donate using Liberapay" src="/img/payments/donate.svg" style={{
                     margin: 0
                 }}/>
             </a>
         </div>
-        <div style={{position: "relative", margin: "5px"}}>
+        {/*<div style={{position: "relative", margin: "5px"}}>
             <img src="/img/payments/bitcoin.svg" style={{
                 // "height: 30px; width: 30px; margin: 0; padding: 0; position: absolute; top: -3px; left: 0;"
                 height: "30px",
@@ -61,7 +61,7 @@ const DonatePage = () => (
                 padding: 0,
                 marginLeft: "35px"
             }}>LTC LYRdNttmhJ1op5EUMiPsHzaZjriJxmL1J8</code>
-        </div>
+        </div>*/}
     </div>
 );
 
