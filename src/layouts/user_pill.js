e6b2f18cc5152549cecb0d378f6d6f334c83e860
Tim Stahel <git@swedneck.xyz>
2018-12-09 00:35:25 +0100

    fix user pill

--- src/layouts/user_pill.js
+++ src/layouts/user_pill.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export default class UserPill extends React.Component {
     componentWillMount() {
         this.setState({profile: {}});
-        axios.get("https://t2bot.io/_matrix/t2bot/user_profile", {params: {user_id: this.props.userId}})
+        axios.get("https://matrix.linuxgaming.life/_matrix/t2bot/user_profile", {params: {user_id: this.props.userId}})
             .then(response => {
                 console.log(response.data);
                 this.setState({profile: response.data});
@@ -13,8 +13,8 @@ export default class UserPill extends React.Component {
 
     render() {
         const displayName = this.state.profile.displayname || this.props.userId;
-        const avatarMxc = this.state.profile.avatar_url || "mxc://t2l.io/UPAtGsUlQYsPwrtAbnSJYvHV";
-        const avatarUrl = "https://t2bot.io/_matrix/media/r0/thumbnail/" + avatarMxc.split('?')[0].substring("mxc://".length) + "?width=120&height=120&animated=true";
+        const avatarMxc = this.state.profile.avatar_url || "mxc://linuxgaming.life/OdOHSWJVvVJWgeLYFzSVWKpv";
+        const avatarUrl = "https://matrix.linuxgaming.life/_matrix/media/r0/thumbnail/" + avatarMxc.split('?')[0].substring("mxc://".length) + "?width=120&height=120&animated=true";
 
         return (
             <a className="t2_UserPill" href={"https://matrix.to/#/" + this.props.userId} title={this.props.userId}>
